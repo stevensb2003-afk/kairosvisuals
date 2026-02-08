9208f603d019cc7f3bd2342d098346ff2534db9e
Steven Sánchez Bonilla <StevenSB.2003@gmail.com>
2026-02-08 08:53:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2320133454-60570",
+  "appId": "1:702999927220:web:5de5c67a338b32e53cfae1",
+  "apiKey": "AIzaSyBJaFwJo7xnYjtWpoHdKFs2jvIQlq9b6os",
+  "authDomain": "studio-2320133454-60570.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "702999927220"
+};
