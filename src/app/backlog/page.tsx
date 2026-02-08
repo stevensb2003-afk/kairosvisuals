1b28d4fe3dcf84fc059ca63b11817a575baee01f
Steven Sánchez Bonilla <StevenSB.2003@gmail.com>
2026-02-08 21:42:11 +0000

    El calendario para escoger fecha de entrega no sirveeee Mejor hazlo de p

--- src/app/backlog/page.tsx
+++ src/app/backlog/page.tsx
@@ -398,6 +398,7 @@ export default function BacklogPage() {
                                                                 mode="single"
                                                                 selected={field.value}
                                                                 onSelect={field.onChange}
+                                                                locale={es}
                                                                 disabled={(date) => date < new Date("1900-01-01")}
                                                                 initialFocus
                                                             />
