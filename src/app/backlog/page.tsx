3aa39716a60b2095c0805ecefd3eb28ca72c1b6f
Steven Sánchez Bonilla <StevenSB.2003@gmail.com>
2026-02-08 10:10:13 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/backlog/page.tsx
+++ src/app/backlog/page.tsx
@@ -358,6 +358,7 @@ export default function BacklogPage() {
                                                         <FormMessage />
                                                     </FormItem>
                                                 )}
+                                            />
                                         )}
                                         <FormField
                                             control={form.control}
